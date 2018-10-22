4d2a6b27bc1af54444e7b0a46b1add9965dc04a3
Brett Groshong <norioz@gmail.com>
2018-10-21 18:29:01 -0700

    Add TODO notes for Scalar.

--- dngin/Scalar.h
+++ dngin/Scalar.h
@@ -1,5 +1,22 @@
 #pragma once
 
+// ----------
+// Scalar
+// ----------
+//min
+//max
+//clamp
+//snap
+//lerp
+//eq / neq - IMPORTANTE
+// ----------
+// Float is equals:
+// absolute - pick some arbitrary epsilon that is significantly small
+// relative - pick the float w / worse error and scale epsilon
+// ulp - does a bitwise comparison
+// https://www.floating-point-gui.de/errors/comparison/
+// https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
+
 struct Scalar {
     float value;
 
