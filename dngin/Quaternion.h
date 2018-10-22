611b4a9c9bae8bba826e9dc02193dcae44a55ec1
Brett Groshong <norioz@gmail.com>
2018-10-21 18:49:53 -0700

    Add Quaternion and Transform impl notes into repo.

---
+++ dngin/Quaternion.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// ----------
+// Quaternion
+// ----------
+//FromAxisAngle
+//ToAxisAngle
+//LookAt
+//concat
+//transformNormal
+//transformVector
+//Quaternion between two vectors
+//slerp
---
+++ dngin/Transform.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// ----------
+// Transform
+// ----------
+//float   scale
+//vector3 position
+//quat    rotation
+//concat
+//toMat4
